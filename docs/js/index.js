cac4048e0338979ae855b9ff7d254306bc90220d
AjaxGb <ajaxgb@gmail.com>
2019-01-29 21:05:35 -0500

    Add starting thread, sounds

--- docs/js/index.js
+++ docs/js/index.js
@@ -1,26 +1,32 @@
 import ForumEngine from './ForumEngine.js';
 import User from './User.js';
 
-const forumEngine = new ForumEngine(10,
-	document.getElementById('thread-template'),
-	document.getElementById('threads-container'),
-);
-
-// Expose for debugging
-window.forumEngine = forumEngine;
-
 (async function() {
+	const forumEngine = new ForumEngine(10,
+		document.getElementById('thread-template'),
+		document.getElementById('threads-container'),
+	);
+
+	// Expose for debugging
+	window.forumEngine = forumEngine;
+	
+	forumEngine.openSounds({
+		ban: 'BanHammer2',
+		lock: 'Lock1',
+		pin: 'Pin2',
+		unpin: 'Pin1',
+	}, 'audio/', '.mp3');
+
 	await forumEngine.load('data/topics.json', 'data/users.json');
 	console.log('Loaded data');
 	
-	forumEngine.createThread(new User('jjFrankz16'), 0, 0,
-		"Hi i'm new");
-	forumEngine.createThread(new User('A'), -0.2, 1,
-		'AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA');
-	forumEngine.createThread(new User('xxXXMurderDemonXXxx'), 0.4, 0,
-		'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum');
-	forumEngine.createThread(new User('TEST'), 1, 1,
-		'TEST').addPost(new User('bbb'), -1, 0.5);
+	const admin = new User('Admin');
+	
+	forumEngine.createThread(admin, 0, 0,
+		'Welcome to the new WeLikePets Forums!', {
+			pinned: true,
+			locked: true,
+		});
 	
 	forumEngine.startSim();
 })();
